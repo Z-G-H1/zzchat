6cdb80ad4925b1447c7aadb42ef28dde5ae8191f
Z-G-H1 <1582696958@qq.com>
2025-05-23 19:06:18 +0800

    修复内存泄露问题

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,13 +7,16 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     // 创建一个centralWidget, 并将其设置为mainWindow的中心部件
-    _login_dlg = new LoginDialog();
+    _login_dlg = new LoginDialog(this);
     setCentralWidget(_login_dlg);
-    _login_dlg->show();
 
     // 创建和注册消息的连接
     connect(_login_dlg, &LoginDialog::switchRegister, this, &MainWindow::SlotSwitchReg);
-    _reg_dlg = new RegisterDialog();
+    _reg_dlg = new RegisterDialog(this);
+
+    _login_dlg->setWindowFlags(Qt::CustomizeWindowHint|Qt::FramelessWindowHint);
+    _reg_dlg->setWindowFlags(Qt::CustomizeWindowHint|Qt::FramelessWindowHint);
+    _reg_dlg->hide();
 
 }
 void MainWindow::SlotSwitchReg(){
@@ -25,13 +28,5 @@ void MainWindow::SlotSwitchReg(){
 MainWindow::~MainWindow()
 {
     delete ui;
-    if(_login_dlg){
-        delete _login_dlg;
-        _login_dlg = nullptr;
-    }
-    if(_reg_dlg){
-        delete _reg_dlg;
-        _reg_dlg = nullptr;
-    }
 }
 
